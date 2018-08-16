b44ea7c3b92163307eebd25d0971c3059aebdc87
Kevin Weidner <weidnerk@gmail.com>
2018-08-16 16:54:17 -0400

    Moving more db code to dsmodels.

--- dsmodels/DataModels.cs
+++ dsmodels/DataModels.cs
@@ -8,14 +8,47 @@ using System.Threading.Tasks;
 
 namespace dsmodels
 {
+    // hate this - StagedListing is nearly identical to PostedListing but has one additional field that does not exist in the PostedListing table
+    public class StagedListing
+    {
+        public string EbaySeller { get; set; }
+        public string EbayItemID { get; set; }
+        public string EbayUrl { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int SourceID { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public string SupplierItemID { get; set; }
+        public string SourceUrl { get; set; }
+        public decimal SupplierPrice { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Pictures { get; set; }
+        public int CategoryID { get; set; }
+        public string PrimaryCategoryID { get; set; }
+        public string PrimaryCategoryName { get; set; }
+        public string ListedItemID { get; set; }
+        public DateTime? Listed { get; set; }
+        public DateTime? Removed { get; set; }
+        public byte ListedQty { get; set; }
+        public bool? InventoryException { get; set; }
+        public byte SoldQty { get; set; }
+
+    }
+
     [Table("PostedListings")]
     public class PostedListing
     {
         public string EbaySeller { get; set; }
-        [Key]
         public string EbayItemID { get; set; }
         public string EbayUrl { get; set; }
+        [Key]
+        [Column(Order = 1)]
         public int SourceID { get; set; }
+        [Key]
+        [Column(Order = 2)]
         public string SupplierItemID { get; set; }
         public string SourceUrl { get; set; }
         public decimal SupplierPrice { get; set; }
@@ -32,11 +65,18 @@ namespace dsmodels
         public byte ListedQty { get; set; }
         public bool? InventoryException { get; set; }
     }
+
+    // Listing is used for the research reporting.
+    // SingleItem is used for the detail page.
+    // Case can be made to just use the Listing class.
+    [Table("Listing")]
     public class Listing
     {
+        [Key]
         public string ItemId { get; set; }
         public string Title { get; set; }
         public string ListingTitle { get; set; }
+        public List<OrderHistory> Orders { get; set; }
         public string EbayUrl { get; set; }
         public string Description { get; set; }
         public decimal SupplierPrice { get; set; }
@@ -46,8 +86,26 @@ namespace dsmodels
         public string PrimaryCategoryID { get; set; }
         public string PrimaryCategoryName { get; set; }
         public byte SourceId { get; set; }
-
         public int Qty { get; set; }
+        public string ListingStatus { get; set; }
+
+    }
+    [Table("OrderHistory")]
+    public class OrderHistory
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string SupplierPrice { get; set; }
+        public string Qty { get; set; }
+        //public string DateOfPurchaseStr { get; set; }
+        public DateTime? DateOfPurchase { get; set; }
+        public int RptNumber { get; set; }
+        public string EbayUrl { get; set; }
+
+        public string ImageUrl { get; set; }
+        public bool ListingEnded { get; set; }
+        public int PageNumber { get; set; }
+        public string ItemId { get; set; }
     }
 
 
--- dsmodels/DataModelsDB.cs
+++ dsmodels/DataModelsDB.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,10 +23,84 @@ namespace dsmodels
 
         public DbSet<PostedListing> PostedListings { get; set; }
 
-        public async Task<PostedListing> GetPostedListing(string listedItemId)
+        //public async Task<PostedListing> GetPostedListing(string listedItemId)
+        //{
+        //    var found = await this.PostedListings.FirstOrDefaultAsync(r => r.ListedItemID == listedItemId);
+        //    return found;
+        //}
+
+        public async Task<PostedListing> GetPostedListing(int sourceID, string supplierItemID)
         {
-            var found = await this.PostedListings.FirstOrDefaultAsync(r => r.ListedItemID == listedItemId);
+            var found = await this.PostedListings.FirstOrDefaultAsync(r => r.SourceID == sourceID && r.SupplierItemID == supplierItemID);
             return found;
+        }
+
+        public async Task<PostedListing> GetPostedListing(string itemId)
+        {
+            var found = await this.PostedListings.FirstOrDefaultAsync(r => r.EbayItemID == itemId);
+            return found;
+        }
+
+
+        public List<StagedListing> GetToList(int categoryId, int listedQty)
+        {
+            List<StagedListing> data =
+                Database.SqlQuery<StagedListing>(
+                "select * from dbo.fnToListFinal(@categoryId, @listedQty)",
+                new SqlParameter("@categoryId", categoryId),
+                new SqlParameter("@listedQty", listedQty))
+            .ToList();
+            return data;
+        }
+
+        public async Task PostedListingSaveAsync(PostedListing listing)
+        {
+            var found = await this.PostedListings.FirstOrDefaultAsync(r => r.SourceID == listing.SourceID && r.SupplierItemID == listing.SupplierItemID);
+            if (found == null)
+                PostedListings.Add(listing);
+            else
+            {
+                found.EbaySeller = listing.EbaySeller;
+                found.Price = listing.Price;
+                found.CategoryID = listing.CategoryID;
+                found.SupplierItemID = listing.SupplierItemID;
+                found.SourceUrl = listing.SourceUrl;
+                found.Pictures = listing.Pictures;
+                found.Title = listing.Title;
+                found.Title = listing.Title;
+                found.EbayUrl = listing.EbayUrl;
+                found.PrimaryCategoryID = listing.PrimaryCategoryID;
+                found.PrimaryCategoryName = listing.PrimaryCategoryName;
+                found.Description = listing.Description;
+                found.SourceID = listing.SourceID;  // 1=sams
+                found.ListedQty = listing.ListedQty;
+                this.Entry(found).State = EntityState.Modified;
+            }
+            await this.SaveChangesAsync();
+        }
+
+        public static PostedListing CopyStagedToPostedListing(StagedListing staged)
+        {
+            var p = new PostedListing();
+
+            p.SourceID = staged.SourceID;
+            p.SupplierItemID = staged.SupplierItemID;
+            p.SourceUrl = staged.SourceUrl;
+            p.SupplierPrice = staged.SupplierPrice;
+            p.Title = staged.Title;
+            p.Price = staged.Price;
+            p.Description = staged.Description;
+            p.Pictures = staged.Pictures;
+            p.CategoryID = staged.CategoryID;
+            p.PrimaryCategoryID = staged.PrimaryCategoryID;
+            p.PrimaryCategoryName = staged.PrimaryCategoryName;
+            p.ListedItemID = staged.ListedItemID;
+            p.Listed = staged.Listed;
+            p.Removed = staged.Removed;
+            p.ListedQty = staged.ListedQty;
+            p.InventoryException = staged.InventoryException;
+
+            return p;
         }
 
     }
