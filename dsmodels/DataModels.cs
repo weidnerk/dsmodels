9b5d8446ddbcb8f5969d5cc0600a01b3d007bf42
Kevin Weidner <weidnerk@gmail.com>
2020-01-18 18:18:07 -0500

    OrderHistory, rename EbayUrl to EbayURL, remove SupplierItemID.

--- dsmodels/DataModels.cs
+++ dsmodels/DataModels.cs
@@ -287,14 +287,14 @@ namespace dsmodels
         [ForeignKey("RptNumber")]
         public virtual SearchHistory SearchHistory { get; set; }
 
-        public string EbayUrl { get; set; } // this field is also present in SellerListing where more formally belongs but useful to have it on TimesSold page
+        public string EbayURL { get; set; } // this field is also present in SellerListing where more formally belongs but useful to have it on TimesSold page
 
         public bool ListingEnded { get; set; }
         public int PageNumber { get; set; }
         [Key]
         public string ItemID { get; set; }
         public int SourceID { get; set; }
-        public string SupplierItemId { get; set; }
+        //public string SupplierItemID { get; set; }
         public string SourceDescription { get; set; }
         public string PrimaryCategoryID { get; set; }
         public string PrimaryCategoryName { get; set; }
@@ -359,32 +359,6 @@ namespace dsmodels
         public string SubCategory { get; set; }
         public string URL { get; set; }
     }
-    //[Table("WalItems")]
-    //public class WalItem
-    //{
-    //    [JsonProperty(PropertyName = "id")]
-    //    public int ID { get; set; }                 // identity field - but still can use this class if no db involved
-    //    public string Title { get; set; }
-    //    [JsonProperty(PropertyName = "description")]
-    //    public string Description { get; set; }
-
-    //    [JsonProperty(PropertyName = "price")]
-    //    public decimal Price { get; set; }
-    //    public string DetailUrl { get; set; }
-    //    public int CategoryID { get; set; }
-    //    public string ItemId { get; set; }
-    //    [JsonProperty(PropertyName = "pictureUrl")]
-    //    public string PictureUrl { get; set; }
-    //    public bool OutOfStock { get; set; }
-    //    public bool ShippingNotAvailable { get; set; }
-    //    [JsonProperty(PropertyName = "fulfilledByWalmart")]
-    //    public bool FulfilledByWalmart { get; set; }
-    //    [JsonProperty(PropertyName = "isVariation")]
-    //    public bool IsVariation { get; set; }
-    //    [JsonProperty(PropertyName = "brand")]
-    //    public string Brand { get; set; }
-    //    public string MPN { get; set; }
-    //}
 
     public class AppIDSelect
     {
@@ -508,7 +482,7 @@ namespace dsmodels
         public string UPC { get; set; } // Walmart doesn't always give you a UPC number so must be nullable in DB
         public string MPN { get; set; }
         public string Description { get; set; }
-        public string ItemID { get; set; }
+        public string ItemID { get; set; }  // this is supplier item number, not to be confused with ebay seller itemID
         public bool OutOfStock { get; set; }
         public bool ShippingNotAvailable { get; set; }
         public virtual List<Listing> Listing { get; set; }
