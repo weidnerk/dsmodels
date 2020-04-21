9f4b201d1cd9db12f7c40e9776102d850d48d3d6
Kevin Weidner <weidnerk@gmail.com>
2020-04-21 15:31:17 -0400

    Fix DeleteListingRecordAsync, SaveChanges after ListingLogs remove.

--- dsmodels/DataModelsDB.cs
+++ dsmodels/DataModelsDB.cs
@@ -402,6 +402,7 @@ namespace dsmodels
                 if (listing != null)
                 {
                     this.ListingLogs.RemoveRange(this.ListingLogs.Where(x => x.ListingID == listing.ID));
+                    await this.SaveChangesAsync();  // if this SaveChanges not here, get an error on nex SaveChanges bcs of FK violation
                     // first remove item specifics
                     this.ListingItemSpecifics.RemoveRange(this.ListingItemSpecifics.Where(x => x.ListingID == listing.ID));
 
